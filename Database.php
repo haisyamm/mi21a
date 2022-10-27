72ecd5a2dde27a011f0914ca31e4e15a830a6252
haisyamm <70573490+haisyamm@users.noreply.github.com>
2022-10-27 09:59:16 +0700

    Add files via upload

---
+++ Database.php
@@ -0,0 +1,21 @@
+<?php
+// htdocs/sigit/Database.php
+class Database {
+	// deklarasi parameter koneksi database
+	private $host   = "localhost";
+	private $uname  = "root";
+	private $pass 	= "";
+	private $db     = "mi21a";
+	
+	public function connect() {
+		// koneksi ke server MySQL
+		$mysqli = new mysqli($this->host, $this->uname, $this->pass, $this->db);	
+		// cek koneksi tersambung atau tidak
+		if ($mysqli->connect_error){
+			echo "Gagal terkoneksi ke database : (".$mysqli->connect_error.")";
+		}  	
+		// nilai kembalian bila koneksi berhasil
+		return $mysqli;
+	}
+}
+?>
